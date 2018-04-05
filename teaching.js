6f4e3a5dc0bae831359d01c9009c45b66ab50f1c
John Passaro <john.a.passaro@gmail.com>
2018-04-05 09:30:48 -0400

    make JS presentable

--- teaching.js
+++ teaching.js
@@ -1,12 +1,9 @@
-//(function() {
-    function surpriseTheUser(text) {
-        var surpriseElement = document.getElementById("surprise");
-        console.log(surpriseElement);
-        surpriseElement.innerHTML = text;
-    }
+window.onload = function () {
+    var eyesImage = document.getElementById("eyes");
+    eyesImage.onclick = function() { surpriseTheUser("You are great!"); };
+};
 
-    window.onload = function () {
-        var eyesImage = document.getElementById("eyes");
-        eyesImage.onclick = function() { surpriseTheUser("You are great!"); };
-    };
-//})()
+function surpriseTheUser(text) {
+    var surpriseElement = document.getElementById("surprise");
+    surpriseElement.innerHTML = text;
+}
